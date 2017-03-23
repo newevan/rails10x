c4c4493347d885b8d9feab7da394f0bc3f6efb22
newevan <evan_bai@126.com>
2017-03-23 15:05:34 +0800

    add user_id to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new]
+  before_action :authenticate_user! , only: [:new, :create]
+
+  def index
     @groups = Group.all
   end
 
@@ -20,14 +22,15 @@ def update
 
   if @group.update(group_params)
   redirect_to groups_path, notice: "电影信息更新成功"
-else
-  render :edit
-end
+  else
+    render :edit
+  end
 end
 
 def create
   @group = Group.new(group_params)
-
+  @group.user = current_user
+  
   if @group.save
     redirect_to groups_path
   else
