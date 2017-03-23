f8d4265f0083af89f3c55259d1a10e8192256815
newevan <evan_bai@126.com>
2017-03-23 14:17:10 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -25,8 +25,18 @@ end
 
 def create
   @group = Group.new(group_params)
-  @group.save
 
+  if @group.save
+    redirect_to groups_path
+  else
+    render :new
+  end
+end
+
+def destroy
+  @group = Group.find(params[:id])
+  @group.destroy
+  flash[:alert] = "电影删除"
   redirect_to groups_path
 end
 
