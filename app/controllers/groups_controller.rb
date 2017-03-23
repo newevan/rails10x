14a7b4e7ffd2e3f4b922ea3f1fe94abaf852bd89
newevan <evan_bai@126.com>
2017-03-23 16:11:48 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,7 +19,7 @@ end
 
 def update
   if @group.update(group_params)
-  redirect_to groups_path, notice: "电影信息更新成功"
+    redirect_to groups_path, notice: "电影信息更新成功"
   else
     render :edit
   end
@@ -38,15 +38,18 @@ end
 
 def destroy
   @group.destroy
-  flash[:alert] = "电影删除成功"
-  redirect_to groups_path
+    redirect_to groups_path, flash[:alert] = "电影删除成功"
 end
 
 
 private
 
 def find_group_and_permission
-  find_group_and_permission
+  @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
 end
 
 def group_params
